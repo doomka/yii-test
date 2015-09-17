d0fcdf09baf4c83019f0c759e0f36fa0cd0e6967
alx <doomka@ukr.net>
2015-09-17 13:46:50 +0300

    test

---
+++ helloworld/protected/controllers/SiteController.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * SiteController is the default controller to handle user requests.
+ */
+class SiteController extends CController
+{
+	/**
+	 * Index action is the default action in a controller.
+	 */
+	public function actionIndex()
+	{
+		echo 'Hello World';
+	}
+
+
+    /**
+     * Displays the contact page
+     */
+    public function actionContact()
+    {
+        $model=new ContactForm;
+        if(isset($_POST['ContactForm']))
+        {
+            $model->attributes=$_POST['ContactForm'];
+            if($model->validate())
+            {
+                $headers="From: {$model->email}\r\nReply-To: {$model->email}";
+                mail(Yii::app()->params['adminEmail'],$model->subject,$model->body,$headers);
+                Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
+                $this->refresh();
+            }
+        }
+        $this->render('contact',array('model'=>$model));
+    }
+
+
+}
